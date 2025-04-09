967bcd491eae52c72c173c2d3db254fa33300574
Samuel Lorand <samuel.lorand@hotmail.com>
2025-04-09 12:59:33 -0300

    Método delete

--- src/main/java/alunoonline/alunoonline/model/Aluno.java
+++ src/main/java/alunoonline/alunoonline/model/Aluno.java
@@ -6,11 +6,12 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "aluno")
+@Data
 @Entity
 public class Aluno {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
@@ -18,38 +19,4 @@ public class Aluno {
     private String cpf;
 
     private String email;
-
-    // Não funcionou com a anotaçao Data
-    
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- src/main/java/alunoonline/alunoonline/service/AlunoService.java
+++ src/main/java/alunoonline/alunoonline/service/AlunoService.java
@@ -3,7 +3,9 @@ package alunoonline.alunoonline.service;
 import alunoonline.alunoonline.model.Aluno;
 import alunoonline.alunoonline.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,4 +28,12 @@ public class AlunoService {
         return alunoRepository.findById(id);
     }
 
+    public void deletarAlunoPorId(Long id) {
+        Optional<Aluno> aluno = alunoRepository.findById(id);
+        if (aluno.isPresent()) {
+            alunoRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aluno não encontrado");
+        }
+    }
 }
