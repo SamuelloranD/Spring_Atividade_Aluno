b5363477995dfb5a34e1a5bd851d0ce1fd8545f9
Samuel Lorand Souto Araujo <samuellorand@academicos.com>
2025-04-15 20:52:32 -0300

    Atualizar aluno

--- src/main/java/alunoonline/alunoonline/model/Aluno.java
+++ src/main/java/alunoonline/alunoonline/model/Aluno.java
@@ -6,7 +6,6 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "aluno")
-@Data
 @Entity
 public class Aluno {
 
@@ -19,4 +18,36 @@ public class Aluno {
     private String cpf;
 
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
