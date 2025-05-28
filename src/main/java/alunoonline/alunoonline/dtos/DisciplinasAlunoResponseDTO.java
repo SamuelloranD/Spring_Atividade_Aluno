5251770f1b47590712b3c6323d7da72b1917c08a
Samuel Lorand <samuel.lorand@hotmail.com>
2025-05-28 12:51:52 -0300

    Correções

--- src/main/java/alunoonline/alunoonline/dtos/DisciplinasAlunoResponseDTO.java
+++ src/main/java/alunoonline/alunoonline/dtos/DisciplinasAlunoResponseDTO.java
@@ -12,54 +12,6 @@ public class DisciplinasAlunoResponseDTO {
     private Double media;
     private MatriculaAlunoStatusEnum matriculaAlunoStatusEnum;
 
-    public String getNomeDisciplina() {
-        return nomeDisciplina;
-    }
-
-    public void setNomeDisciplina(String nomeDisciplina) {
-        this.nomeDisciplina = nomeDisciplina;
-    }
-
-    public String getNomeProfessor() {
-        return nomeProfessor;
-    }
-
-    public void setNomeProfessor(String nomeProfessor) {
-        this.nomeProfessor = nomeProfessor;
-    }
-
-    public Double getNota1() {
-        return nota1;
-    }
-
-    public void setNota1(Double nota1) {
-        this.nota1 = nota1;
-    }
-
-    public Double getNota2() {
-        return nota2;
-    }
-
-    public void setNota2(Double nota2) {
-        this.nota2 = nota2;
-    }
-
-    public Double getMedia() {
-        return media;
-    }
-
-    public void setMedia(Double media) {
-        this.media = media;
-    }
-
-    public MatriculaAlunoStatusEnum getMatriculaAlunoStatusEnum() {
-        return matriculaAlunoStatusEnum;
-    }
-
-    public void setMatriculaAlunoStatusEnum(MatriculaAlunoStatusEnum matriculaAlunoStatusEnum) {
-        this.matriculaAlunoStatusEnum = matriculaAlunoStatusEnum;
-    }
-
     public void setStatus(MatriculaAlunoStatusEnum status) {
         this.matriculaAlunoStatusEnum = status;
     }
--- src/main/java/alunoonline/alunoonline/model/Disciplina.java
+++ src/main/java/alunoonline/alunoonline/model/Disciplina.java
@@ -26,35 +26,4 @@ public class Disciplina {
     @JoinColumn(name = "professor_id")
     private Professor professor;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    public void setCargaHoraria(Integer cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-    }
-
-    public Professor getProfessor() {
-        return professor;
-    }
-
-    public void setProfessor(Professor professor) {
-        this.professor = professor;
-    }
 }
